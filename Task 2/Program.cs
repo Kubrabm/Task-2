53602bf641bfe9f4b9a4be518c2f127f96bc34b4
kubrabm <kubrabm@code.edu.az>
2022-12-28 17:18:18 +0400

    Add project files.

---
+++ Task 2/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //////1
+            //Console.WriteLine("Daxil edilen ededin cut yerde duran reqemlerinin kvadratinin cemi hesablayan proqram");
+            //Console.Write("Ededi daxil edin (eded 8 reqemden artiq olmamalidir):");
+            //int eded = int.Parse(Console.ReadLine());
+            //int a = eded % 10;
+            //int b = eded / 100 % 10;
+            //int c = eded / 10000 % 10;
+            //int d = eded / 1000000 % 10;
+            //int q = q = (a * a) + (b * b) + (c * c) + (d * d);
+            //Console.WriteLine($"VDaxil edilen ededin cut yerde duran reqemlerinin kvadratinin cemi {q}");
+
+
+
+
+            //////2
+            //Console.WriteLine("Verilmis 5 reqemli ededin evveline 4 sonuna 2 reqemi artiran proqram");
+            //Console.Write("Ededi daxil edin (eded 5 reqemli olmalidir):");
+            //int n = int.Parse(Console.ReadLine());
+            //int a = n * 10 + 2 + 4000000;
+            //Console.WriteLine($"{a}");
+
+
+
+
+
+            //////3.1
+            //Console.WriteLine("Daxil edilen reqemler ve sıra nomreleri cemini hesablayan proqram");
+            //Console.WriteLine("A ededini daxil edin :");
+            //int A = int.Parse(Console.ReadLine());
+            //Console.WriteLine("A1 ededininin sira nomresini daxil edin :");
+            //int A1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("B ededini daxil edin :");
+            //int B = int.Parse(Console.ReadLine());
+            //Console.WriteLine("B1 ededininin sira nomresini daxil edin :");
+            //int B1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("C ededini daxil edin :");
+            //int C = int.Parse(Console.ReadLine());
+            //Console.WriteLine("C1 ededininin sira nomresini daxil edin :");
+            //int C1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("D ededini daxil edin :");
+            //int D = int.Parse(Console.ReadLine());
+            //Console.WriteLine("D1 ededininin sira nomresini daxil edin :");
+            //int D1 = int.Parse(Console.ReadLine());
+            ///
+            /// int F = (A * A1) + (B * B1) + (C * C1) + (D * D1);
+            ///
+            //////3.2
+            ////int eded = int.Parse(Console.ReadLine()); 
+            ////Console.WriteLine("Ededi daxil edin :");
+            ////int A = eded / 1000;
+            ////int B = eded / 100%10;
+            ////int C = eded /10 %10;
+            ////int D = eded % 10;
+
+            ////int F = (A * 1) + (B * 2) + (C * 3) + (D * 4);
+
+            ////Console.WriteLine($"Daxil edilen reqemler ve sıra nomreleri cemi:{F}");
+        }
+    }
+}
